7c66cd178492400f3a1b88b75228ff303144cb17
aliasgar-kaeed-challawala <aliasgarkcdlsl@gmail.com>
2021-07-06 15:10:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const p1score = document.querySelector("#p1display");
+const p2score = document.querySelector("#p2display");
+
+const p1button = document.querySelector("p1button");
+const p2button = document.querySelector("p2button");
